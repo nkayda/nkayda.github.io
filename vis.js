5fb21e8e62f165aa2f7970e8ff961b0400ca02bc
Natalie Kayda <nataliekayda708@gmail.com>
2026-02-12 18:03:45 -0800

    Added vega-lite API dependencies, added functional visualization, updated styling

--- vis.js
+++ vis.js
@@ -1,3 +1,36 @@
+async function fetchData() {
+  const data = await d3.csv("/datasets/videogames_wide.csv");
+  return data;
+}
+
+async function render(viewID, spec) {
+  const result = await vegaEmbed(viewID, spec);
+  result.view.run();
+}
+
+fetchData().then(async (data) => {
+  const vlSpec = vl
+    .markBar()
+    .data(data)
+    .encode(
+      vl.y().fieldN("Platform").sort("-x"),
+      vl.x().fieldQ("Global_Sales").aggregate("sum")
+    )
+    .width("container")
+    .height(400)
+    .toSpec();
+
+  render("#view", vlSpec);
+});
+
+
+
+
+
+
+
+// --- ASSIGNMENT 1 -----------------------------------------------------------
+
 let circles = document.querySelectorAll('circle');
 
 circles.forEach((circle) => {
