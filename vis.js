b553c39b86dd88c8dc863d3edc50c1eb623e64f1
Natalie Kayda <nataliekayda708@gmail.com>
2026-02-14 14:31:27 -0800

    Completed Global Sales by Genre and Platform visualizations

--- vis.js
+++ vis.js
@@ -8,38 +8,99 @@ async function render(viewID, spec) {
   result.view.run();
 }
 
+// Specified colours for the genres, per built-in JS colours
+genreColours = [
+  "crimson",
+  "darkcyan",
+  "cornsilk",
+  "darkseagreen",
+  "deeppink",
+  "lavendar",
+  "lightblue",
+  "darkred",
+  "sandybrown",
+  "slategray",
+  "turquoise",
+  "darkmagenta",
+];
+
 fetchData().then(async (data) => {
-  const vlSpec = vl
+  const globalSalesByGenrePlatform = vl
     .markBar()
     .data(data)
     .encode(
-      vl.y().fieldN("Platform").sort("-x"),
-      vl.x().fieldQ("Global_Sales").aggregate("sum")
+      vl.x().fieldN("Platform").sort("-y"),
+      vl.y()
+        .fieldQ("Global_Sales")
+        .aggregate("sum")
+        .axis({ title: "Global Sales (millions):" }),
+      vl.color().fieldN("Genre").scale({ range: genreColours }),
+
+      vl.tooltip([
+        { field: "Genre", type: "nominal", title: "Genre:" },
+        {
+          field: "Global_Sales",
+          type: "quantitative",
+          aggregate: "sum",
+          title: "Global Sales (millions):",
+        },
+      ]),
     )
     .width("container")
     .height(400)
     .toSpec();
 
-  render("#view", vlSpec);
+  const globalSalesByGenrePlatformV2 = vl
+    .markBar()
+    .data(data)
+    .transform(
+      vl.filter('datum["Genre"] == "Puzzle"'),
+      vl.filter('datum["Publisher"] == "Nintendo"'),
+    )
+    .encode(
+      vl.y()
+        .fieldQ("Global_Sales")
+        .aggregate("sum")
+        .axis({ title: "Global Sales of Puzzle Games (millions)" }),
+      vl.x().fieldO("Platform").sort("-y"),
+      vl.color().fieldN("Name").sort("-y")
+        .legend({
+          title: "Nintendo Puzzle Game Title",
+          symbolLimit: 100,
+          columns: 3,
+          labelLimit: 300,
+          orient: "bottom",
+        }),
+
+      vl.tooltip([
+        { field: "Genre", type: "nominal", title: "Genre:" },
+        { field: "Name", type: "nominal", title: "Name:" },
+        {
+          field: "Global_Sales",
+          type: "quantitative",
+          aggregate: "sum",
+          title: "Global Sales (millions):",
+        },
+      ]),
+    )
+    .width("container")
+    .height(400)
+    .toSpec();
+
+  render("#view", globalSalesByGenrePlatform);
+  render("#view2", globalSalesByGenrePlatformV2);
 });
-
-
-
-
-
-
 
 // --- ASSIGNMENT 1 -----------------------------------------------------------
 
-let circles = document.querySelectorAll('circle');
+let circles = document.querySelectorAll("circle");
 
 circles.forEach((circle) => {
-  circle.addEventListener('click', () => {
-    if (circle.getAttribute("fill") == 'red'){
-        circle.setAttribute("fill", 'black');
-    }
-    else {
-        circle.setAttribute('fill', 'red');
+  circle.addEventListener("click", () => {
+    if (circle.getAttribute("fill") == "red") {
+      circle.setAttribute("fill", "black");
+    } else {
+      circle.setAttribute("fill", "red");
     }
   });
 });
