b8c9836b760138f2d7bd98a85a0970a4de2ebbef
RCC1783 <75766592+RCC1783@users.noreply.github.com>
2026-04-19 13:52:12 -0700

    added permanant markers for population and income

--- final_project/income_map.js
+++ final_project/income_map.js
@@ -19,33 +19,37 @@ var inc_year_slider = document.getElementById('income_years_slider');
 var inc_bounds = L.geoJSON(null);
 
 var city_income = [
-    {name: 'City of Vancouver', '2001': 42026, '2006': 47299, '2011': 56113, '2016': 65327, '2021': 82000},
-    // {name: 'City of Richmond', '2001': null, '2006': null, '2011': null, '2016': null, '2021': null},
-    // {name: 'City of Surrey', '2001': null, '2006': null, '2011': null, '2016': null, '2021': null},
-    {name: 'City of Burnaby', '2001': 44754, '2006': 50205, '2011': 56136, '2016': 64737, '2021': 83000},
-    {name: 'City of New Westminster', '2001': 40784, '2006': 48773, '2011': 54664, '2016': 64695, '2021': 82000},
-    {name: 'City of Coquitlam', '2001': 52657, '2006': 59294, '2011': 67787, '2016': 74383, '2021': 92000},
-    {name: 'City of Port Coquitlam', '2001': 59926, '2006': 65731, '2011': 72563, '2016': 84096, '2021': 102000},
-    {name: 'City of Port Moody', '2001': 64932, '2006': 74527, '2011': 79918, '2016': 92922, '2021': 115000},
+    {name: 'City of Vancouver', '2001': 42026, '2006': 47299, '2011': 56113, '2016': 65327, '2021': 82000, coords: [49.260833, -123.113889]},
+    {name: 'City of Richmond', '2001': 50060, '2006': 53489, '2011': 60479, '2016': 65241, '2021': 79000, coords: [49.166667, -123.133333]},
+    {name: 'City of Surrey', '2001': 53957, '2006': 60168, '2011': 67702, '2016': 77494, '2021': 98000, coords:[49.16547389937119, -122.81818665235177]},
+    {name: 'City of Burnaby', '2001': 44754, '2006': 50205, '2011': 56136, '2016': 64737, '2021': 83000, coords: [49.266667, -122.966667]},
+    {name: 'City of New Westminster', '2001': 40784, '2006': 48773, '2011': 54664, '2016': 64695, '2021': 82000, coords: [49.206944, -122.911111]},
+    {name: 'City of Coquitlam', '2001': 52657, '2006': 59294, '2011': 67787, '2016': 74383, '2021': 92000, coords:[49.29861288226988, -122.7462376038563]},
+    {name: 'City of Port Coquitlam', '2001': 59926, '2006': 65731, '2011': 72563, '2016': 84096, '2021': 102000, coords:[49.23417004952855, -122.78388193771077]},
+    {name: 'City of Port Moody', '2001': 64932, '2006': 74527, '2011': 79918, '2016': 92922, '2021': 115000, coords:[49.2985952911707, -122.86175954847108]},
 ]
 
 function setupCityIncFeatures(feature, layer){
     var cityObj = city_income.filter(city => {return (city.name == feature.properties.FullName)});
     if(cityObj.length == 0) {layer.bindPopup(`<h3>${feature.properties.FullName}</h3><p>undef</p>`); return; }
     var inc = cityObj[0][`${inc_year_slider.value}`];
-    layer.bindPopup(`<h3>${feature.properties.FullName}</h3><h4>Income: <span style="font-weight: 500; color: #008A07">$${inc}</span></h4>`);
+    // layer.bindPopup(`<h3>${feature.properties.FullName}</h3><h4>Income: <span style="font-weight: 500; color: #008A07">$${inc}</span></h4>`);
 
-    var min = 9999999;
-    var max = 0;
+    var min = 42026;
+    var max = 115000;
     
-    city_income.forEach(city => {
-        if(city[inc_year_slider.value] > max) max = city[inc_year_slider.value];
-        if(city[inc_year_slider.value] < min) min = city[inc_year_slider.value];
-    });
+    // city_income.forEach(city => {
+    //     if(city[inc_year_slider.value] > max) max = city[inc_year_slider.value];
+    //     if(city[inc_year_slider.value] < min) min = city[inc_year_slider.value];
+    // });
 
     layer.setStyle({
         fillOpacity: ((inc-min)/(max-min))
     })
+
+    L.circle(cityObj[0].coords, {radius: 200, color: 'white', fillColor: 'white', fillOpacity: 1, weight: 5})
+        .bindTooltip(`<h3>${feature.properties.FullName}</h3><h4>Income: <span style="font-weight: 500; color: #008A07">$${inc}</span></h4>`, { permanent: true })
+        .addTo(inc_boundriesGroup);
 }
 function addCities_inc(){
     fetch(boundriesAPI_URL).then(response => {
--- final_project/population_cities_map.js
+++ final_project/population_cities_map.js
@@ -22,21 +22,21 @@ var cities_year_slider = document.getElementById('cities_years_slider');
 var city_bounds = L.geoJSON(null);
 
 var city_populations = [
-    {name: 'City of Vancouver', '2001': 572394, '2006': 600281, '2011': 622467, '2016': 664913, '2021': 696031},
-    {name: 'City of Richmond', '2001': 171269, '2006': 180313, '2011': 194973, '2016': 207610, '2021': 221178},
-    {name: 'City of Surrey', '2001': 363639, '2006': 409229, '2011': 482063, '2016': 543495, '2021': 597141},
-    {name: 'City of Burnaby', '2001': 202682, '2006': 210145, '2011': 229632, '2016': 244567, '2021': 261800},
-    {name: 'City of New Westminster', '2001': 57222, '2006': 60688, '2011': 67949, '2016': 74761, '2021': 82943},
-    {name: 'City of Coquitlam', '2001': 117942, '2006': 118650, '2011': 130540, '2016': 146146, '2021': 155554},
-    {name: 'City of Port Coquitlam', '2001': 53171, '2006': 54028, '2011': 57013, '2016': 61522, '2021': 64205},
-    {name: 'City of Port Moody', '2001': 24958, '2006': 28626, '2011': 33900, '2016': 35065, '2021': 34987},
+    {name: 'City of Vancouver', '2001': 572394, '2006': 600281, '2011': 622467, '2016': 664913, '2021': 696031, coords: [49.260833, -123.113889]},
+    {name: 'City of Richmond', '2001': 171269, '2006': 180313, '2011': 194973, '2016': 207610, '2021': 221178, coords: [49.166667, -123.133333]},
+    {name: 'City of Surrey', '2001': 363639, '2006': 409229, '2011': 482063, '2016': 543495, '2021': 597141, coords:[49.16547389937119, -122.81818665235177]},
+    {name: 'City of Burnaby', '2001': 202682, '2006': 210145, '2011': 229632, '2016': 244567, '2021': 261800, coords: [49.266667, -122.966667]},
+    {name: 'City of New Westminster', '2001': 57222, '2006': 60688, '2011': 67949, '2016': 74761, '2021': 82943, coords: [49.206944, -122.911111]},
+    {name: 'City of Coquitlam', '2001': 117942, '2006': 118650, '2011': 130540, '2016': 146146, '2021': 155554, coords:[49.29861288226988, -122.7462376038563]},
+    {name: 'City of Port Coquitlam', '2001': 53171, '2006': 54028, '2011': 57013, '2016': 61522, '2021': 64205, coords:[49.23417004952855, -122.78388193771077]},
+    {name: 'City of Port Moody', '2001': 24958, '2006': 28626, '2011': 33900, '2016': 35065, '2021': 34987, coords:[49.2985952911707, -122.86175954847108]},
 ]
 
 function setupCityFeatures(feature, layer){
     var cityObj = city_populations.filter(city => {return (city.name == feature.properties.FullName)});
     if(cityObj.length == 0) {layer.bindPopup(`<h3>${feature.properties.FullName}</h3><p>undef</p>`); return; }
     var pop = cityObj[0][`${cities_year_slider.value}`];
-    layer.bindPopup(`<h3>${feature.properties.FullName}</h3><h4>Population: <span style="font-weight: 500; color: #008A07">${pop}</span></h4>`);
+    // layer.bindPopup(`<h3>${feature.properties.FullName}</h3><h4>Population: <span style="font-weight: 500; color: #008A07">${pop}</span></h4>`, {permanent: true});
 
     var min = 24958;
     var max = 696031;
@@ -45,11 +45,13 @@ function setupCityFeatures(feature, layer){
     //     if(city[cities_year_slider.value] > max) max = city[cities_year_slider.value];
     //     if(city[cities_year_slider.value] < min) min = city[cities_year_slider.value];
     // });
-    console.log(`${cities_year_slider.value} - ${min}:${max}`)
+    // console.log(`${cities_year_slider.value} - ${min}:${max}`)
 
     layer.setStyle({
         fillOpacity: ((pop-min)/(max-min))
     })
+
+    
 }
 function addCities(){
     fetch(boundriesAPI_URL).then(response => {
@@ -69,6 +71,11 @@ function addCities(){
 }
 
 addCities(); 
+city_populations.forEach(city => {
+    L.circle(city.coords, {radius: 200, color: 'white', fillColor: 'white', fillOpacity: 1, weight: 5})
+        .bringToFront().bindTooltip(`<h3>${city.name}</h3><h4>Population: <span style="font-weight: 500; color: #008A07">${city[`${cities_year_slider.value}`]}</span></h4>`, { permanent: true })
+        .addTo(boundriesGroup)
+})
 
 cities_year_slider.oninput = function (){
     boundriesGroup.eachLayer(layer => {
@@ -76,4 +83,10 @@ cities_year_slider.oninput = function (){
     })
 
     addCities();
+
+    city_populations.forEach(city => {
+        L.circle(city.coords, {radius: 200, color: 'white', fillColor: 'white', fillOpacity: 1, weight: 5})
+            .bindTooltip(`<h3>${city.name}</h3><h4>Population: <span style="font-weight: 500; color: #008A07">${city[`${cities_year_slider.value}`]}</span></h4>`, { permanent: true })
+            .addTo(boundriesGroup);
+    })
 }
