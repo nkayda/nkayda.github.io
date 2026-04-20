7204727b7ab8737fe6d9178c42fe17146bb7226a
Natalie Kayda <nataliekayda708@gmail.com>
2026-04-19 19:36:08 -0700

    Reorganized content, renamed files to be more descriptive

--- final_project/scroll.js
+++
@@ -1,8 +0,0 @@
-const scrollProgress = document.getElementById('scroll-progress');
-const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-const train = document.getElementById('train-nav');
-
-window.addEventListener('scroll', () => {
-    const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-    scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
-});
